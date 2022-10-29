7dd3245ec53fff71a21ade5bca3e9dbd119b2b5e
Shobha Murthy <shobha.murthy@carestream.com>
2022-10-29 05:45:32 -0500

    initial creation

---
+++ bitcoin-model/retrain.py
@@ -0,0 +1,111 @@
+import os
+from os import listdir
+from os.path import isfile, join
+import dotenv
+import mlflow
+import numpy as np
+import pandas as pd
+import requests
+import tweepy as tw
+from datasets import load_dataset, load_metric
+from transformers import (AutoModelForSequenceClassification, AutoTokenizer,
+                          DataCollatorWithPadding, TextClassificationPipeline,
+                          Trainer, TrainingArguments)
+
+from utils.io import load_yaml
+
+#let's load the params
+config_dict = load_yaml('./config/btc-config.yaml')
+model_name = config_dict['model_name']
+sentiment = {'bearish':0, 'neutral':1, 'bullish':2}
+input_dir = config_dict['input_dir']
+output_dir = config_dict['output_dir']
+epochs = int(config_dict['epochs'])
+#Load env variables
+dotenv.load_dotenv(dotenv.find_dotenv())
+# set mlflow env variables
+os.environ['MLFLOW_EXPERIMENT_NAME'] = "mlflow-trainer_cryptobert"
+os.environ['MLFLOW_FLATTEN_PARAMS'] = "1"
+
+class LoadTweets:
+  def __init__(self, config_dict):
+    bearer_token = os.getenv('BEARER_TOKEN')
+    self.client = tw.Client(bearer_token,  return_type=requests.Response, wait_on_rate_limit=True)
+    self.query = config_dict['v2_query']
+
+  def get_tweets(self, max_tweets=10):
+    tweets = self.client.search_recent_tweets(query=self.query, 
+                                              tweet_fields=['text'], max_results=max_tweets).json()['data']
+    return [tweet['text'] for tweet in tweets]
+    
+  def load_dataset(self, data_files, get_tokens_function, seed=12):
+    dataset = load_dataset("csv", data_files=data_files, delimiter=",")
+    print(dataset)
+    tokenized_dataset = dataset.map(get_tokens_function, batched=True)
+    train = tokenized_dataset["train"].shuffle(seed=seed)
+    test = tokenized_dataset["test"].shuffle(seed=seed)
+    validation = tokenized_dataset.get("validation")
+    if validation:
+      return train, test, validation.shuffle(seed=seed)
+    else:
+      return train, test, None
+
+class RoBERTa_sentiment():
+    def __init__(self, name: str):
+        self.model_name = name
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name, use_fast=True)
+        self.model = AutoModelForSequenceClassification.from_pretrained(self.model_name, num_labels = 3)
+        self.pipe = TextClassificationPipeline(model=self.model, tokenizer=self.tokenizer)
+
+    def predict(self, tweets):
+        preds = self.pipe(tweets)
+        df = pd.DataFrame(preds)
+        df.insert(0, "tweet", tweets, True)
+        return df
+    
+    def get_token(self, row):
+        return self.tokenizer(row["tweet"], padding="max_length", truncation=True)
+
+
+
+model = RoBERTa_sentiment(model_name)
+LoadData = LoadTweets(config_dict)
+
+train_files = [join(input_dir+'/train/', item) for item in listdir(input_dir+'/train/') if isfile(join(input_dir+'/train/', item))]
+test_files = [join(input_dir+'/test/', item) for item in listdir(input_dir+'/test/') if isfile(join(input_dir+'/test/', item))]
+data_files={"train": train_files, "test": test_files}
+
+train, test, _ = LoadData.load_dataset(data_files, model.get_token)
+
+data_collator = DataCollatorWithPadding(tokenizer=model.tokenizer)
+
+metric = load_metric("accuracy")
+
+# We can add more metrics here
+def compute_metrics(pred):
+    logits, labels = pred
+    predictions = np.argmax(logits, axis=-1)
+    return metric.compute(predictions=predictions, references=labels)
+
+mlflow.autolog()
+
+training_args = TrainingArguments(
+  output_dir=output_dir,
+    overwrite_output_dir = True,
+    num_train_epochs=epochs,
+    save_strategy='epoch',
+    logging_steps=500,
+)
+
+trainer = Trainer(
+    model=model.model, 
+    args=training_args, 
+    train_dataset=train, 
+    eval_dataset=test,
+    tokenizer=model.tokenizer,
+    data_collator=data_collator,
+    compute_metrics=compute_metrics,
+)
+
+trainer.save_model()
+mlflow.end_run()
